1f3d19b5513ed210e855456f395044e2e5367f95
Martin <info@mgdsventures.com>
2017-10-25 01:22:47 +1100

    loading page

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
 import { firebase } from './firebase/firebase';
 
+import LoadingPage from './components/LoadingPage';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -29,7 +31,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
